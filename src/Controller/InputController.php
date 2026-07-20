e4b6e3852a01c50ab4dcd9cde3fb7769c4f5dfad
Damoclès <esteban.mignotte@gmail.com>
2026-07-20 14:59:40 +0200

    Création Input Controller et Repo
    FInalisation du controller et ajout du Repo suite à l'oublie de ce dernier

--- src/Controller/InputController.php
+++ src/Controller/InputController.php
@@ -2,16 +2,71 @@
 
 namespace App\Controller;
 
+use App\Entity\Input;
+use App\Repository\InputRepository;
+use App\Repository\ProjectsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-
 
 final class InputController extends AbstractController
 {
-    #[Route('/input', name: 'app_input', methods:['POST'])]
-    public function index(): Response
-    {
+    #[Route('/input', name: 'app_input_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager, ProjectsRepository $projectsRepository): JsonResponse {
 
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return $this->json(['error' => 'Données JSON invalides.']);
+        }
+
+
+        if (!isset($data['tag_id'])) {
+            return $this->json(['error' => 'Le champ tag_id est obligatoire.']);
+        }
+
+        $project = $projectsRepository->find($data['tag_id']);
+        if (!$project) {
+            return $this->json(['error' => 'Project non trouvé pour ce tag_id.']);
+        }
+
+
+        $input = new Input();
+        $input->setTagId($project);
+
+        $input->setIp($data['ip']);
+        $input->setPageName($data['page_name']);
+        $input->setUri($data['uri']);
+        $input->setIsLogin($data['isLogin']); // La vue dois être forcément Login ou nn ?? //
+
+
+        $entityManager->persist($input);
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'Input créé avec succès',
+            'id' => $input->getId()
+        ]);
+    }
+
+    #[Route('/input', name: 'app_input_list', methods: ['GET'])]
+    public function index(InputRepository $inputRepository): JsonResponse
+    {
+        $inputs = $inputRepository->findAll();
+
+        $data = array_map(function (Input $input) {
+            return [
+                'id' => $input->getId(),
+                'tag_id' => $input->getTagId() ? $input->getTagId()->getId() : null,
+                'ip' => $input->getIp(),
+                'page_name' => $input->getPageName(),
+                'uri' => $input->getUri(),
+                'isLogin' => $input->isLogin(),
+            ];
+        }, $inputs);
+
+        return $this->json($data);
     }
 }
