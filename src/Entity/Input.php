9dc5763e84ee1238977da7a367713fe3cc1eec0f
Damoclès <esteban.mignotte@gmail.com>
2026-07-20 13:16:11 +0200

    Ajout Entity Input

---
+++ src/Entity/Input.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InputRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InputRepository::class)]
+class Input
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Projects $tag_id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $ip = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $page_name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $uri = null;
+
+    #[ORM\Column]
+    private ?bool $isLogin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTagId(): ?Projects
+    {
+        return $this->tag_id;
+    }
+
+    public function setTagId(?Projects $tag_id): static
+    {
+        $this->tag_id = $tag_id;
+
+        return $this;
+    }
+
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    public function setIp(string $ip): static
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    public function getPageName(): ?string
+    {
+        return $this->page_name;
+    }
+
+    public function setPageName(string $page_name): static
+    {
+        $this->page_name = $page_name;
+
+        return $this;
+    }
+
+    public function getUri(): ?string
+    {
+        return $this->uri;
+    }
+
+    public function setUri(string $uri): static
+    {
+        $this->uri = $uri;
+
+        return $this;
+    }
+
+    public function isLogin(): ?bool
+    {
+        return $this->isLogin;
+    }
+
+    public function setIsLogin(bool $isLogin): static
+    {
+        $this->isLogin = $isLogin;
+
+        return $this;
+    }
+}
