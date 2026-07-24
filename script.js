6844f72ccd6d668e778ba349c254d0822f7236f8
Nonyx_69 <noa.guilhot.pro@gmail.com>
2026-07-24 16:57:59 +0200

    pour project (pas sur)
    Co-Authored-By: Damoclès <esteban.mignotte@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 (async () => {
 
+    // j'ai trouver un truc comme ca mais je suis pas sur je sait meme pas comment le tester
+
+    // const project = new URL(document.currentScript.src).searchParams.get("project");
+
     fetch('http://localhost:8000/input', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
+            // project: project
             project: 'TyroTag-d531274a380d0aea1b23aa256629cc896a5f50a7e0fa6',
             ip: (await (await fetch('https://api.ipify.org?format=json')).json()).ip,
             page_name: document.title,
